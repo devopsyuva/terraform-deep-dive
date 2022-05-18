cfa2eb0965df10714bf8416293e1dfc8b371d33c
sudheer reddy duba <info@sudheerdevops.com>
2022-05-18 13:01:06 +0530

    Updated s3 bucjet policy for_each meta-arguments

---
+++ T02-Terraform-resources/02-Terraform-resources-behavior/02-resource-meta-arguments/03-for_each/v1-without-vars/s3_for_each_policy.tf
@@ -0,0 +1,38 @@
+/*
+This configuration will create 3 s3 buckets which names
+provided with key-value on AWS provider
+*/
+resource "aws_s3_bucket" "s3demobucket" {
+  for_each = {
+    dev  = "s3-dev-bucket"
+    test = "s3-test-bucket"
+    qa   = "s3-qa-bucket"
+  }
+  bucket = "${each.key}-${each.value}"
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"PublicReadGetObject",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject","s3:GetObjectVersion"],
+      "Resource":["arn:aws:s3:::${each.key}-${each.value}/*"]
+    }
+  ]
+}
+POLICY
+
+  tags = {
+    Name      = "${each.key}-${each.value}"
+    Eachvalue = "${each.value}"
+    Eachkey   = "${each.key}"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  for_each = {for k, v in aws_s3_bucket.s3demobucket : k => v.id}
+  bucket = "${each.value}"
+  acl    = "public-read"
+}
