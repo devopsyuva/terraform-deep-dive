0c86327cba3463e084a20333839d57c8112c6cd7
Sudhams Reddy Duba <dubareddy.383@gmail.com>
2024-11-12 07:15:42 +0530

    Update ami-datasource.tf

--- T04-Terraform-datasources/ami-datasource.tf
+++ T04-Terraform-datasources/ami-datasource.tf
@@ -25,7 +25,7 @@ data "aws_ami" "ubuntu_ami" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
   filter {
     name   = "root-device-type"
